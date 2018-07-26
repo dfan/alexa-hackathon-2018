a73163538283e916e00ef5708b0134e900d6300a
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-07-26 12:52:04 -0700

    Fixed lambda function for getting ingredients

--- recipe-generator/index.js
+++ recipe-generator/index.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
-require('dotenv').config()
+require('dotenv').config();
 
 exports.handler = (event, context, callback) => {
   fetch(`https://api.edamam.com/search?q=${event.query}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
     .then(res => res.json())
-    .then(json => callback(json.hits[0].recipe.ingredientLines))
+    .then(json => callback(null, json.hits[0].recipe.ingredientLines))
     .catch(err => new Error(err));
 }
 
