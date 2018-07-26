ae715a4849e8315e34bf0725e8c21700d51c9bb6
Matthew Lam <cheukyin@usc.edu>
2018-07-26 14:47:09 -0700

    Add SendRecipeIntent

--- dream-cooker/index.js
+++ dream-cooker/index.js
@@ -4,6 +4,9 @@ const HAVE_INGREDIENTS_QUESTION = "HAVE_INGREDIENTS";
 const HEAR_RECIPE_QUESTION = "HEAR_QUESTION";
 const QUESTION_STATE_ATTRIBUTE = "QUESTION_STATE";
 const APP_ID = "amzn1.ask.skill.e770bd0d-a891-4563-874c-b6716c36ea04";
+const MEDIATYPE_EMAIL_SLOT = "email";
+const MEDIATYPE_TEXT_SLOT = "slot";
+const MEDIATYPE_TODO_SLOT = "to do list";
 const handlers = {
     // Route new requests to Launch Request
     'NewSession': function(){
@@ -12,7 +15,7 @@ const handlers = {
     'LaunchRequest': function () {
         this.attributes[QUESTION_STATE_ATTRIBUTE] = HAVE_INGREDIENTS_QUESTION;
 
-        this.emit('GetNewFactIntent');
+        this.emit(':responseReady');
     },
     'MakeFoodIntent': function () {
         this.emit(':responseReady');
@@ -38,6 +41,25 @@ const handlers = {
 
         }
         this.emit(':responseReady');
+    },
+    'SendRecipeIntent': function(){
+        const slotValue = this.event.request.intent.slots.mediaType.value;
+        if(slotValue === MEDIATYPE_EMAIL_SLOT){
+        
+        }
+        else if (slotValue === MEDIATYPE_TEXT_SLOT){
+
+        }
+        else if (slotValue === MEDIATYPE_TODO_SLOT){
+
+        }
+        else{
+            const slotToElicit = 'mediaType';
+            const speechOutput = 'Choose the method to send your recipe. '
+                + 'It could be email, text, or to do list.';
+            const repromptOutput = speechOutput;
+            this.emit(':elicitSlot', slotToElicit, speechOutput, repromptOutput);
+        }
     },
     'AMAZON.HelpIntent': function () {
         this.emit(':responseReady');
